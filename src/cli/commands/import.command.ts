76292d5f16ade3bfbd9bfb6a10e309cebafb5b1c
kiipod <iamkir90@hotmail.com>
2023-09-28 12:49:26 +0300

    Рефакторинг команды с импортом данных

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,26 +1,33 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
-import chalk from 'chalk';
+import { createOffer, getErrorMessage } from '../../shared/helpers/index.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
     return '--import';
   }
 
-  public execute(...params: string[]): void {
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
+  public async execute(...params: string[]): Promise<void> {
     const [filename] = params;
     const fileReader = new TSVFileReader(filename.trim());
 
-    try {
-      fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (error) {
-      if (!(error instanceof Error)) {
-        throw error;
-      }
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
 
-      console.error(chalk.red(`Can't import data from file: ${filename}`));
-      console.error(chalk.red(`Details: ${error.message}`));
+    try {
+      await fileReader.read();
+    } catch (error) {
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,78 +1,36 @@
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
-import { Offer, UserRole, CitiesName, Comforts, HouseType } from '../../types/index.js';
-import chalk from 'chalk';
+import { createReadStream } from 'node:fs';
+import EventEmitter from 'node:events';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(
-    private readonly filename: string
-  ) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error(chalk.red('File was not read'));
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(
-        ([
-          title,
-          description,
-          date,
-          city,
-          preview,
-          photos,
-          isPremium,
-          isFavorite,
-          rating,
-          houseType,
-          rooms,
-          guests,
-          price,
-          comforts,
-          name,
-          email,
-          avatar,
-          password,
-          role,
-          latitude,
-          longitude,
-        ]) => ({
-          title: title,
-          description: description,
-          date: new Date(date),
-          city: { name: CitiesName[city as keyof typeof CitiesName] },
-          preview: preview,
-          photos: photos.split(';'),
-          isPremium: isPremium === 'true',
-          isFavorite: isFavorite === 'true',
-          rating: Number.parseInt(rating, 10),
-          houseType: HouseType[houseType as keyof typeof HouseType],
-          rooms: Number.parseInt(rooms, 10),
-          guests: Number.parseInt(guests, 10),
-          price: Number.parseInt(price, 10),
-          comforts: comforts.split(';').map((comfort) => Comforts[comfort as keyof typeof Comforts]),
-          author: {
-            name: name,
-            email: email,
-            avatar: avatar,
-            password: password,
-            role: UserRole[role as keyof typeof UserRole],
-          },
-          location: {
-            latitude: Number.parseFloat(latitude),
-            longitude: Number.parseFloat(longitude),
-          },
-        })
-      );
+    this.emit('end', importedRowCount);
   }
 }
