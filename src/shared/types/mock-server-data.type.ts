87311a3be21600be42e445f61565d8252dc91a91
kiipod <iamkir90@hotmail.com>
2023-09-28 12:41:12 +0300

    Добавляет тестовые данные и тип для генерации данных

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,22 @@
+export type MockServerData = {
+    title: string[];
+    description: string[];
+    date: string[];
+    city: string[];
+    preview: string[];
+    photos: string[];
+    isPremium: string[];
+    isFavorite: string[];
+    rating: string[];
+    rooms: string[];
+    guests: string[];
+    price: string[];
+    comforts: string[];
+    name: string[];
+    email: string[];
+    avatar: string[];
+    password: string[];
+    role: string[];
+    latitude: string[];
+    longitude: string[];
+};
