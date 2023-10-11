9d05d076c6230941eb174abad52cb6b4abb389fc
kiipod <iamkir90@hotmail.com>
2023-10-11 18:39:33 +0300

    Добавляет тип сортировки запросов

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,5 +7,6 @@ export { User, UserRole } from './user.type.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './location.type.js';
+export * from './sort-type.enum.js';
 
 
