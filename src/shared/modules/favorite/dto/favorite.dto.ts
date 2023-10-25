222b637147f8c32b3c934739f9e404bde484973e
kiipod <iamkir90@hotmail.com>
2023-10-25 11:09:36 +0300

    Добавляет поддержку миддлваров в Favorite

--- src/shared/modules/favorite/dto/favorite.dto.ts
+++ src/shared/modules/favorite/dto/favorite.dto.ts
@@ -1,4 +1,10 @@
+import { IsMongoId } from 'class-validator';
+import { FavoriteMessages } from './favorite.messages.js';
+
 export class FavoriteDto {
+  @IsMongoId({ message: FavoriteMessages.authorId.invalidFormat })
   userId: string;
+
+  @IsMongoId({ message: FavoriteMessages.offerId.invalidFormat })
   offerId: string;
 }
--- src/shared/modules/favorite/favorite.controller.ts
+++ src/shared/modules/favorite/favorite.controller.ts
@@ -1,36 +1,71 @@
 import { inject } from 'inversify';
-import {BaseController, HttpError, HttpMethod} from '../../libs/rest/index.js';
+import {
+  BaseController,
+  HttpError,
+  HttpMethod,
+  ValidateObjectIdMiddleware,
+  ValidateDtoMiddleware,
+  DocumentExistsMiddleware
+} from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Request, Response } from 'express';
-import { DefaultFavoriteService, CreateFavoriteRequest, DeleteFavoriteRequest, FavoriteRdo } from './index.js';
+import {
+  DefaultFavoriteService,
+  CreateFavoriteRequest,
+  DeleteFavoriteRequest,
+  FavoriteRdo,
+  FavoriteDto
+} from './index.js';
 import { fillDTO } from '../../helpers/index.js';
 import { StatusCodes } from 'http-status-codes';
+import { DefaultOfferService } from '../offer/index.js';
 
 export class FavoriteController extends BaseController {
   constructor(
         @inject(Component.Logger) protected readonly logger: Logger,
         @inject(Component.FavoriteService) private readonly favoriteService: DefaultFavoriteService,
+        @inject(Component.OfferService) private readonly offerService: DefaultOfferService,
   ) {
     super(logger);
 
     this.logger.info('Register routes for FavoriteController…');
 
-    this.addRoute({ path: '/favorite', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/favorite/:offerId/', method: HttpMethod.Post, handler: this.create });
-    this.addRoute({ path: '/favorite/:offerId/', method: HttpMethod.Delete, handler: this.destroy });
+    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
+
+    this.addRoute({
+      path: '/:offerId',
+      method: HttpMethod.Post,
+      handler: this.create,
+      middlewares: [
+        new ValidateObjectIdMiddleware('offerId'),
+        new ValidateDtoMiddleware(FavoriteDto)
+      ]
+    });
+
+    this.addRoute({
+      path: '/:offerId',
+      method: HttpMethod.Delete,
+      handler: this.delete,
+      middlewares: [
+        new DocumentExistsMiddleware(this.offerService, 'Offer', 'offerId'),
+        new ValidateObjectIdMiddleware('offerId'),
+        new ValidateDtoMiddleware(FavoriteDto)
+      ]
+    });
   }
 
+  // Метод отвечает за поиск всех избранных предложений у пользователя
   public async index(_req: Request, res: Response): Promise<void> {
-    // проверка пользователя
     const userId = '785578ywrw3yf4w';
     const userFavorites = await this.favoriteService.findByUserId(userId);
     const responseData = fillDTO(FavoriteRdo , userFavorites);
+
     this.ok(res, responseData);
   }
 
+  // Метод отвечает за добавление предложения в список избранных
   public async create({ body }: CreateFavoriteRequest, res: Response): Promise<void> {
-    // проверка объявления у пользователя
     const existsUserOffer = await this.favoriteService.findByUserOfferId(body);
 
     if (existsUserOffer) {
@@ -45,8 +80,8 @@ export class FavoriteController extends BaseController {
     this.created(res, fillDTO(FavoriteRdo, result));
   }
 
-  public async destroy({ body }: DeleteFavoriteRequest, res: Response): Promise<void> {
-    // проверка объявления у пользователя
+  // Метод отвечает за удаление предложения из списка избранных
+  public async delete({ body }: DeleteFavoriteRequest, res: Response): Promise<void> {
     const existsUser = await this.favoriteService.findByUserOfferId(body);
 
     if (existsUser) {
